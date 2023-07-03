a3915ae4a0bf11ec95aad10a49254753d6d1d805
Ivan Varyukhin <98258568+ivan-varyukhin@users.noreply.github.com>
2023-07-03 21:33:41 +0300

    fix: PEP8

--- articles/models.py
+++ articles/models.py
@@ -1,23 +1,31 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from mixins.mixins import TimeStampedMixin, UUIDMixin
+from mixins.models import TimeStampedMixin, UUIDMixin
 
 
 class Article(UUIDMixin, TimeStampedMixin):
+    class Meta:
+        ordering = ['-created_at']
+
     image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=255)
     text = models.TextField()
     source_name = models.CharField(max_length=255, null=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     source_link = models.URLField(max_length=2047, null=True, blank=True)
     is_published = models.BooleanField(default=False)
     views_count = models.IntegerField(default=0, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     tags = models.ManyToManyField('Tags')
     likes = models.ManyToManyField('Likes')
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.reading_time and self.text:
+            self.reading_time = self.calculate_reading_time()
+        super().save(*args, **kwargs)
 
     def calculate_reading_time(self):
         words = len(self.text.split())
@@ -27,14 +35,6 @@ class Article(UUIDMixin, TimeStampedMixin):
     def increment_views_count(self):
         self.views_count += 1
         self.save()
-
-    class Meta:
-        ordering = ['-created_at']
-
-    def save(self, *args, **kwargs):
-        if not self.reading_time and self.text:
-            self.reading_time = self.calculate_reading_time()
-        super().save(*args, **kwargs)
 
     def get_likes_count(self):
         return self.likes.count()
