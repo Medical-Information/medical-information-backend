c8ff38579ee3dc8998f2a0f658874d7a7511430c
Ivan Varyukhin <98258568+ivan-varyukhin@users.noreply.github.com>
2023-07-03 20:49:53 +0300

    fix: PEP8

--- mixins/mixins.py
+++ mixins/mixins.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.utils import timezone
 
+
 class TimeStampedMixin(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
