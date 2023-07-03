bb8f9a017316004320ddeb6264aa82680d20cb67
Ivan Varyukhin <98258568+ivan-varyukhin@users.noreply.github.com>
2023-07-03 17:49:52 +0300

    feat: adding swagger

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from .swagger import schema_view
+   
+   urlpatterns = [
+       path('v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+       path('v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   ]
+
+   urlpatterns += staticfiles_urlpatterns()
