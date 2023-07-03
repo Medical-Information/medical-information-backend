c5fb095196c990911c5e3c1927814f8dac3c7681
Ivan Varyukhin <98258568+ivan-varyukhin@users.noreply.github.com>
2023-07-03 21:37:50 +0300

    fix: PEP8

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,19 @@
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
 
-from .swagger import schema_view
-   
-   urlpatterns = [
-       path('v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-       path('v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-   ]
+from api.swagger import schema_view
 
-   urlpatterns += staticfiles_urlpatterns()
+urlpatterns = [
+    path(
+        'v1/swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
+    path(
+        'v1/redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
+]
+
+urlpatterns += staticfiles_urlpatterns()
