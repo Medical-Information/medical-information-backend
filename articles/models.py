f4fa4b025499a12da76961da78e1b84deb85cca8
Ivan Varyukhin <98258568+ivan-varyukhin@users.noreply.github.com>
2023-07-03 21:03:57 +0300

    fix: import

--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from mixins.models import TimeStampedMixin, UUIDMixin
+from mixins.mixins import TimeStampedMixin, UUIDMixin
 
 
 class Article(UUIDMixin, TimeStampedMixin):
