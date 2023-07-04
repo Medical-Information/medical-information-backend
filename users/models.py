340e4e44054772f308455d7e8d667e887b9d4647
Станислав Андрющенко <StAndryushchenko@yandex.ru>
2023-07-04 13:03:55 +0300

    fix err import

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from .managers import UserManager
+from users.managers import UserManager
 
 from core.models import TimeStampedMixin, UUIDMixin
 
