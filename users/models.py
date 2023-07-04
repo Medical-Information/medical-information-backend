1c09d5a2a7200603a7ed2b7bb691dbe18f7482a3
Станислав Андрющенко <StAndryushchenko@yandex.ru>
2023-07-04 12:59:34 +0300

    flake8+core

--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 from .managers import UserManager
+
 from core.models import TimeStampedMixin, UUIDMixin
 
 
@@ -13,7 +14,7 @@ class User(TimeStampedMixin, UUIDMixin, AbstractUser):
     пользовательских атрибутов и методов.
     """
     class Meta:
-        ordering = ['id']
+        ordering = ['uuid']
         verbose_name = _('user')
         verbose_name_plural = _('users')
 
