2785fbfbf21c295c82d0cf2ccc79de3031385fc8
Станислав Андрющенко <StAndryushchenko@yandex.ru>
2023-07-04 13:38:57 +0300

    fix bug

--- users/models.py
+++ users/models.py
@@ -1,9 +1,11 @@
+from typing import List
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from users.managers import UserManager
 
 from core.models import TimeStampedMixin, UUIDMixin
+from users.managers import UserManager
 
 
 class User(TimeStampedMixin, UUIDMixin, AbstractUser):
@@ -19,7 +21,7 @@ class User(TimeStampedMixin, UUIDMixin, AbstractUser):
         verbose_name_plural = _('users')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS: List[str] = []
 
     USER = 'user'
     MODER = 'moderator'
