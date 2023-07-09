30a3ceb8eb3070bc2f3df202c32ae95b167a82b2
Ivan Varyukhin <ivan@varyukhin.ru>
2023-07-09 16:53:44 +0300

    fix: PEP8

--- users/admin.py
+++ users/admin.py
@@ -33,7 +33,9 @@ class UserAdmin(admin.ModelAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'role', 'is_active', 'is_staff', 'is_superuser'),
+            'fields': (
+				'email', 'password1', 'password2', 'role', 'is_active', 'is_staff', 'is_superuser',
+			),
         }),
     )
 
