0fd0e6db4194d7bd51c07ff58fcc34c9979394ba
Ivan Varyukhin <98258568+ivan-varyukhin@users.noreply.github.com>
2023-07-09 16:28:45 +0300

    fix: PEP8

--- users/admin.py
+++ users/admin.py
@@ -13,15 +13,15 @@ class UserAdmin(admin.ModelAdmin):
 		 'role',
 		 'is_active',
 		 'is_staff',
-    )
+	)
 	list_filter = ('role', 'is_active', 'is_staff')
 	search_fields = ('email', 'first_name', 'last_name')
 	fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('role',)}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
+		(None, {'fields': ('email', 'password')}),
+		(_('Personal Info'), {'fields': ('role',)}),
+		(_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+		(_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+	)
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
