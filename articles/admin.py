507cf549482d7985f9964b8f86403d0d247eedf6
Алексей Андреев <siner1982@gmail.com>
2023-07-09 21:43:31 +0500

    add: TagForm в админке, проверка на дублировании тега в родительских и дочерних тегах.

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.db import transaction
+from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
 
 from articles.models import Article, Tag
 
@@ -17,9 +21,34 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ('author',)
 
 
+class TagForm(ModelForm):
+    """Форма модели Tag. Нужна для проверки дублирования
+    в родительских и дочерних тегах.
+    """
+
+    def check_tags_matches(self, instance):
+        for parent in instance.parents.all():
+            if parent in instance.children.all():
+                return False, parent.name
+        return True, None
+
+    def clean(self):
+        ok = False
+        with transaction.atomic(savepoint=True, durable=False):
+            ok, double = self.check_tags_matches(self.save(commit=True))
+            transaction.set_rollback(True)
+        if not ok:
+            raise ValidationError(
+                _('The tag %(double)s cannot be parent and child') % {'double': double},
+            )
+        return super().clean()
+
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     """Отображение модели Tag в админ-панели."""
+
+    form = TagForm
 
     prepopulated_fields = {'slug': ('name',)}
     search_fields = ('name',)
@@ -28,12 +57,10 @@ class TagAdmin(admin.ModelAdmin):
         ('children', admin.RelatedOnlyFieldListFilter),
     )
     list_display = (
-        'id',
         'name',
         'slug',
     )
     list_display_links = (
-        'id',
         'name',
         'slug',
     )
