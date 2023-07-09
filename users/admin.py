abb7f3d27d23675c72cca73123ab7eec3e9914b8
Ivan Varyukhin <98258568+ivan-varyukhin@users.noreply.github.com>
2023-07-09 16:22:37 +0300

    fix: add filters in user admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from users.models import User
 
@@ -9,8 +10,32 @@ class UserAdmin(admin.ModelAdmin):
         'email',
         'first_name',
         'last_name',
+		 'role',
+		 'is_active',
+		 'is_staff',
     )
+	list_filter = ('role', 'is_active', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('role',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+			    'email',
+				'password1',
+				'password2',
+				'role',
+				'is_active',
+				'is_staff',
+				'is_superuser',
+			),
+        }),
+    )	
 
 
 admin.site.register(User, UserAdmin)
