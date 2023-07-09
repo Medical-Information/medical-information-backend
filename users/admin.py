c2cf82acc83b5cabc049cbef3bc7d32bb32b4193
Ivan Varyukhin <ivan@varyukhin.ru>
2023-07-09 16:48:50 +0300

    fix: PEP8

--- users/admin.py
+++ users/admin.py
@@ -5,45 +5,45 @@ from users.models import User
 
 
 class UserAdmin(admin.ModelAdmin):
-	list_display = (
-		'pk',
-		'email',
-		'first_name',
-		'last_name',
-		'role',
-		'is_active',
-		'is_staff',
-	)
-	list_filter = (
-		'role',
-		'is_active',
-		'is_staff',
-	)
-	search_fields = (
-		'email',
-		'first_name',
-		'last_name',
-	)
-	fieldsets = (
-		(None, {'fields': ('email', 'password')}),
-		(_('Personal Info'), {'fields': ('role',)}),
-		(_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
-		(_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-	)
-	add_fieldsets = (
+    list_display = (
+        'pk',
+        'email',
+        'first_name',
+        'last_name',
+        'role',
+        'is_active',
+        'is_staff',
+    )
+    list_filter = (
+        'role',
+        'is_active',
+        'is_staff',
+    )
+    search_fields = (
+        'email',
+        'first_name',
+        'last_name',
+    )
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('role',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
 		(None, {
-			'classes': ('wide',),
-			'fields': (
-				'email',
-				'password1',
-				'password2',
-				'role',
-				'is_active',
-				'is_staff',
-				'is_superuser',
-			),
-		}),
-	)
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'role',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
 
 
 admin.site.register(User, UserAdmin)
