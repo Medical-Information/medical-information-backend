ab7ade0d11c2b03aeceeae7d461ab5774da5205d
Станислав Андрющенко <StAndryushchenko@yandex.ru>
2023-07-12 10:19:20 +0300

    Добавил приложение likes, внес изменения в модель articles

--- likes/models.py
+++ likes/models.py
@@ -3,6 +3,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
 from likes.managers import LikeDislikeManager
 
 User = get_user_model()
