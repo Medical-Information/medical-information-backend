f26ca83b08bffa65298dce7eef78159b1c975178
Сергей Гриценко <gritsenko.serge.2013@yandex.ru>
2023-07-12 13:28:19 +0300

    feat (article, api): article_favotite post/delete

--- api/serializers.py
+++ api/serializers.py
@@ -46,8 +46,8 @@ class ArticleSerializer(ModelSerializer):
 
     class Meta:
         model = Article
-        fields = '__all__'  # TODO заменить на кортеж полей
-        read_only_fields = ('created_at', 'updated_at', 'is_favorited')
+        fields = '__all__'
+        read_only_fields = ('created_at', 'updated_at')
 
     image = Base64ImageField()
-    is_favorited = BooleanField()
+    is_favorited = BooleanField(read_only=True)
