36396fc7e60e3738c6a4630d926827cfc80a4057
Станислав Андрющенко <StAndryushchenko@yandex.ru>
2023-07-23 19:56:19 +0300

    добавил CORS

--- stethoscope/settings.py
+++ stethoscope/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'mptt',
+    'corsheaders',
     'users.apps.UsersConfig',
     'api.apps.ApiConfig',
     'articles.apps.ArticlesConfig',
@@ -40,12 +41,21 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# доступ со всех доменов
+CORS_ORIGIN_ALLOW_ALL = True
+
+# доступ c определенных доменов
+# CORS_ALLOWED_ORIGINS = [
+#     '...',
+# ]
 
 ROOT_URLCONF = 'stethoscope.urls'
 
