32caa9b33cea7535f68583d8b8df5634bfe72f1b
Сергей Гриценко <gritsenko.serge.2013@yandex.ru>
2023-08-03 03:17:41 +0300

    hot fix

--- users/models.py
+++ users/models.py
@@ -1,6 +1,4 @@
-﻿from typing import List
-
-from django.contrib.auth.models import AbstractUser
+﻿from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinLengthValidator, RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -32,7 +30,7 @@ class User(TimeStampedMixin, UUIDMixin, AbstractUser):
     """
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS: List[str] = []
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     first_name = models.CharField(
         _('first name'),
