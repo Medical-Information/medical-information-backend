5b28d3b4ac7dc64c4dba7291e258de04c73efd02
AlexanderUp <alexander.uperenk0@yandex.ru>
2023-08-04 22:42:41 +0300

    Refactoring mailing model/admin

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 
-from mailings.models import TopArticles
+from mailings.models import TopArticle
 
 
-@admin.register(TopArticles)
-class TopArticlesAdmin(admin.ModelAdmin):
+@admin.register(TopArticle)
+class TopArticleAdmin(admin.ModelAdmin):
     list_display = (
-        'articles',
+        'article',
+        'article_author',
     )
+
+    def get_queryset(self, request):
+        return TopArticle.objects.select_related('article', 'article__author')
+
+    @admin.display()
+    def article_author(self, obj):
+        return obj.article.author
